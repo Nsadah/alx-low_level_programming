bd64f5f91c3492801028f92465733df7502c1876
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-08-26 23:38:43 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,8 @@
+
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 #include <unistd.h>
-#define RED "\e[0;31m"
-#define NC "\e[0m"
 int main(){
 fwrite("[ERROR] : No string argument provided!\n", stderr);
 printf("and that piece of art is useful" - Dora Korpar, 2015-10-19");
