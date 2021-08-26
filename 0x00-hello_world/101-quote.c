ffb78957048c726229b6f098fcd013e155b67a8d
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-08-26 23:37:55 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,23 +2,11 @@
 #include <string.h>
 #include <stdio.h>
 #include <unistd.h>
-
 #define RED "\e[0;31m"
 #define NC "\e[0m"
-
-
 int main(){
-
-    if (argc != 2) {
-        fwrite("[ERROR] : No string argument provided!\n", 39, 1, stderr);
-        exit(EXIT_FAILURE);
-    }
-
-    char *str = malloc(strlen(argv[1]) + 1);
-    strcpy(str, argv[1]);
-
-    printf("str: %s\n", str);
-
-    free(str);
-    exit(EXIT_SUCCESS);
+fwrite("[ERROR] : No string argument provided!\n", stderr);
+printf("and that piece of art is useful" - Dora Korpar, 2015-10-19");
+return (1);
+ /* Program returns 1 */
 }
