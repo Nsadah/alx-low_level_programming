1e3ba7842f144cfca9dd3828681508bbf8631cd0
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-08-26 21:03:15 +0300

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,22 @@
-#!/bin/bash
+/**
+ * main - print with proper grammar, but the outcome is a piece of art,
+ *
+ * Return: 0
+ */
+#include<stdio.h>
+int main() /*main prints text*/
+{
+    int intType;
+    float floatType;
+    double doubleType;
+    char charType;
+
+ /* sizeof evaluates the size of a variable */
+    printf("Size of int: %zu bytes\n", sizeof(intType));
+    printf("Size of float: %zu bytes\n", sizeof(floatType));
+    printf("Size of double: %zu bytes\n", sizeof(doubleType));
+    printf("Size of char: %zu byte\n", sizeof(charType));
+/* Description: This is where the printing happens */
+    return 0;
+/* Description: This returns 0 */
+}
