2b3f8a9b34f8affe878fee183108c2379bc3e958
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-08-27 00:21:17 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 /**
- * main - print and that piece of art is useful" - Dora Korpar, 2015-10-19
- *
- * Return: 0
- */
+* main - print and that piece of art is useful" - Dora Korpar, 2015-10-19
+*
+* Return: 0
+*/
 #include<stdio.h>
 int main(void) /*main prints text*/
 {
 printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 /* Description: This is where the printing happens */
 return (1);
-/* Description: This returns 0 */
+  /* Description: This returns 0 */
 }
