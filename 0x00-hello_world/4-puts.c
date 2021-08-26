fc4870e95952c7b457c4c212684c8f1063c46556
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-08-26 20:06:09 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,6 +5,13 @@
 int main(void)
 {
 char put[] = "Programming is like building a multilingual puzzle\n";
-puts(put);/*This is where the pronting happens*/
-return (0);/*This returns 0*/
+puts(put);
+ /**
+ *This is where the pronting happens
+ */
+return (0);
+ /**
+ *
+ This returns 0
+ */
 }
