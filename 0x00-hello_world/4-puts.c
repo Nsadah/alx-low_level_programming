b1ba1caceffc61d02d9ed6cdbaafc0cca6dd1a85
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-08-26 18:53:58 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,6 @@
 #include<stdio.h>
-int main()
+main()
 {
-    //string initialisation
-    char Mystr[] = "C and C++";
-     
-    puts(Mystr); //writing the string to stdout
-     
+    puts("Programming is like building a multilingual puzzle);    
     return 0;
 }
