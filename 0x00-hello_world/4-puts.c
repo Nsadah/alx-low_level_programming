dc3754f9e31ccebc71956a25a536f68465906cf6
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-08-26 20:07:10 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,18 @@
 /**
-*This is my first c program
-*/
+ * op_add - Makes the sum of two numbers
+ * @arg1: First operand
+ * @arg2: Second operand
+ *
+ * Description: This is a longer description.
+ * Don't forget that a line should not exceed 80 characters.
+ * But you're totally free to use several lines to properly
+ * describe your function
+ * Return: The sum of the two parameters
+ */
+int op_add(int arg1, int arg2)
+{
+	return (arg1 + arg2);
+}
 #include<stdio.h>
 int main(void)
 {
