3cea31e7bbade04719ccdfdb55425743816fc08e
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-08-27 00:00:48 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,9 +4,8 @@
 *
 * Return: Always 0 (success)
 */
-int main(void) /* main prints a text */
+int main(void)
 {
 printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
-/* Program returns 1 */
 }
