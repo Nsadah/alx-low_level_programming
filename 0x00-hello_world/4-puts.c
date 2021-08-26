faa12936a38d7f8fea7f0cf0de1df49d3a1b949f
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-08-26 20:24:30 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
 #include<stdio.h>
 int main(void)
 {
-char put[] = "Programming is like building a multilingual puzzle\n";
+char put[] = "{"Programming is like building a multilingual puzzle\n}";
 puts(put);
 /**
 *This is where the pronting happens
