107090cdd8d887160a039cec35331e24ce7a978b
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-08-26 23:56:28 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,11 @@
 /**
- * main - print with proper grammar, but the outcome is a piece of art,
+ * main - and that piece of art is useful" - Dora Korpar, 2015-10-19
  *
- * Return: 0
+ * Return: 1
  */
-#include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
-#include <unistd.h>
-int main(){
-fwrite("[ERROR] : No string argument provided!\n", stderr);
+int main(void) /* main prints a text */
+{
 printf("and that piece of art is useful" - Dora Korpar, 2015-10-19");
 return (1);
 /* Program returns 1 */
