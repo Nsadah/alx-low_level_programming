6135a1c32321199a7496347c3be8f6546bf9070d
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-08-26 19:41:23 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(void)
 {
-char put[]= ""Programming is like building a multilingual puzzle\n";
+char put[]= "Programming is like building a multilingual puzzle\n";
 puts(put);
 return 0;
 }
