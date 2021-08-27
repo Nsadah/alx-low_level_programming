c67ca40b8cb4afceea8ce9bbd098e5523d36e648
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-08-28 01:54:37 +0300

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,8 +10,8 @@ int main(void)
 int i, j, k;
 for (i = '0'; i <= '9'; i++)
 {
-for (j = '0'; j <= '9'; j++) 	 
-{  
+for (j = '0'; j <= '9'; j++)
+{
 for (k = 0; k <= '9'; k++)
 {
 if (i < j && j < k)
@@ -19,7 +19,7 @@ if (i < j && j < k)
 putchar (i);
 putchar (j);
 putchar (k);
-if (i!='7'|| ((i == '7' )&& (j !='8')) || (j != '8' ||((j == '8') && k !='9')))
+if (i != '7'|| ((i == '7') && (j != '8')) || (j != '8' || ((j == '8') && k != '9')))
 {
 putchar (',');
 putchar (' ');
