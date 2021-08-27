6d2b270ee8bc7158607bb50b9aa41e4f79da9bba
Nsadah <michealnsadahal@gmail.com>
2021-08-27 09:34:23 -0700

    C

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+/**
+ * main - prints to string
+ * Description: Prints alphabet in lowercase
+ * Return: 0
+ */
+#include<stdio.h>
+int main(void)
+{
+int x;
+for (x = 0; x < 10; x++)
+printf("%d", x);
+putchar('\n');
+return (0);
+}
