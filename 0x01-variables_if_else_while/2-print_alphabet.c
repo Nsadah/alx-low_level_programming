01c873475351f27e5652db59228bff9bc015cb13
Nsadah <michealnsadahal@gmail.com>
2021-08-27 06:12:35 -0700

    C

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints to string
+ * Description: Prints alphabet in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+int ch;
+clrscr();
+for (ch= ‘a’; ch<= ‘z’; ch++)
+{
+putchar(ch);
+putchar (‘\n’);
+getch();
+}
