5cc049c5c3bfb6d9a3a0043f9419e5383e02c3ed
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-08-27 13:11:44 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+
 /**
  * main - prints to string
  * Description: Prints "and that piece of art is useful.." without puts
@@ -8,16 +9,6 @@
 
 int main(void)
 {
-char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-long l = 59;
-long fd = 1;
-long syscall = 1;
-long ret = 0;
-__asm__ ("syscall"
-: "=a" (ret)
-: "a" (syscall),
-"D" (fd),
-"S" (s),
-"d" (l));
+fprintf( stderr, "%s \n", "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 30);
 return (1);
 }
