2a01eadcb097fd512fab315b8e9ae4677c5b3e87
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-08-27 22:46:01 +0300

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,18 +4,17 @@
  * Return: 0
  */
 #include<stdio.h>
-#include <stdlib.h> 
+#include <stdlib.h>
 #include <string.h>
 int main(void)
 {
 int i;
-
-for(i = 0; i < 10 ; i++)
-{	
-putchar((i)+'0');
+for (i = 0; i < 10 ; i++)
+{
+putchar ((i) + '0');
 if(i <= 8)
 {
-putchar(',');
+putchar (',');
 putchar (' ');
 }
 }
