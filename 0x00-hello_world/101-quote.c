65fc26b074824e82aa6a95b4541c81a181bb3e6b
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-08-27 13:15:18 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
-
-
 /**
  * main - prints to string
  * Description: Prints "and that piece of art is useful.." without puts
  * Return: 1
  */
-
 int main()
 {
-printf("%s \n", "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 30);
+printf("%s", "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 30);
 return (1);
 }
