c425365f54538de0599109a135e2587154991284
Nsadah <michealnsadahal@gmail.com>
2021-08-27 11:52:54 -0700

    C

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+/**
+ * main - prints to string
+ * Description: Prints alphabet in lowercase
+ * Return: 0
+ */
+#include<stdio.h>
+int main(void)
+{
+int x=0;
+int i;
+while(x <10)
+{
+for (x = 0; x < 10; x++)
+putchar((x)+ '0');
+}
+for (i = 'a'; i <'g'; i++)
+putchar (i);
+putchar ('\n');
+return (0);
+}
