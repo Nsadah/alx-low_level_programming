4960f632b2d0625062251bb45f22cec671ccf31d
Nsadah <michealnsadahal@gmail.com>
2021-08-27 10:30:47 -0700

    C

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+/**
+ * main - prints to string
+ * Description: Prints alphabet in lowercase
+ * Return: 0
+ */
+#include<stdio.h>
+int main()
+{
+int ch;
+for(ch='z';ch>='a';ch--)
+putchar(ch);
+putchar('\n');
+return 0;
+}
