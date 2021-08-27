68c8285fa2e5e1f6fb0c5a8457b05c56e5ed88e5
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-08-27 20:22:06 +0300

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,11 +7,11 @@
 int main(void)
 {
 int x = 0;
-while(x < 10)
+while (x < 10)
 {
-for(x = 0; x < 10; x++)	
-putchar((x)+'0');
-putchar('\n');
+for (x = 0; x < 10; x++)	
+putchar ((x)+'0');
+putchar ('\n');
 }
 return (0);
 }
