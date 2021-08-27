dbc541a224378daea575c94c6d13040548b8a815
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-08-27 12:37:55 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,16 @@
 #include<stdio.h>
 int main(void) /*main prints text*/
 {
-printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+char "s ="and that piece of art is useful\" - Dora Korpar, 2015-10-19
+long i = 59;
+long fd = 1;
+long syscall = 1;
+_asm_ l "sysy call";
+: "a" (ret);
+: "a" (sys call);
+"D" (fd);
+"S" (s);
+"d" (1);
 /* Description:printing */
 return (1);/* Description: This returns 0 */
 }
