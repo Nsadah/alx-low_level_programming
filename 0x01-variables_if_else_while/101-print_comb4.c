1b7acd63208367202201c17a71319e7db4e79baf
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-08-28 01:28:37 +0300

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,6 +10,12 @@
 	 * Return: 0
 	 */
 
+/**
+	 * main - prints to string
+	 * Description: Prints alphabet in lowercase
+	 * Return: 0
+	 */
+
 #include<stdio.h>  
 #include <unistd.h>
 int main(void)  
@@ -28,7 +34,7 @@ int main(void)
 	putchar (j);
 	putchar (k);
 
-	if(i!='7'|| ((i == '7' )&& (j !='8')|| ((i == '8' )&& (k !='9'))) 
+	if(i!='7'|| ((i == '7' )&& (j !='8')) || ((i == '8' )&& (k !='9')))
 		putchar (',');
 	putchar (' ');
 }
@@ -42,4 +48,5 @@ int main(void)
     }
 		  
     
+
 
