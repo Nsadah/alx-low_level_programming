5e69fe7ad109224b6086778fa96e4d64904be0da
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-08-28 01:18:28 +0300

    Update 101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+/**
+	 * main - prints to string
+	 * Description: Prints alphabet in lowercase
+	 * Return: 0
+	 */
+
+#include<stdio.h>  
+#include <unistd.h>
+int main(void)  
+{  
+    int i, j, k;  
+  
+    for(i = '0'; i <= '9'; i++)  
+    {  
+    for(j = '0'; j <= '9'; j++) 	 
+	{  
+	for (k =0; k <= '9'; k++)
+    {
+	if(i < j && j < k)
+	{
+	putchar (i);
+	putchar (j);
+	putchar (k);
+	
+	
+	putchar (',');
+	putchar (' ');	
+	}  			
+    }  
+    }
+    }
+		putchar('\n');
+	 return 0; 	  
+    }  
+
