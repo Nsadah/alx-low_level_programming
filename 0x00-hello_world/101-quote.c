ee790a92ceb64e4fb26cf6a66d3fdb6e8fcec368
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-08-27 12:58:06 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -14,10 +14,10 @@ long fd = 1;
 long syscall = 1;
 long ret = 0;
 __asm__ ("syscall"
-		: "=a" (ret)
-		: "a" (syscall),
-		"D" (fd),
-		"S" (s),
-		"d" (l));
+: "=a" (ret)
+: "a" (syscall),
+"D" (fd),
+"S" (s),
+"d" (l));
 return (1);
 }
