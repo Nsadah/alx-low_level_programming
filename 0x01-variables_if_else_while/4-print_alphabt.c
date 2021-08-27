ab607f6cf1b5c06f3a371eb2897c297a8ba7ec70
Nsadah <michealnsadahal@gmail.com>
2021-08-27 09:00:22 -0700

    C

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+/**
+ * main - prints to string
+ * Description: Prints alphabet in lowercase
+ * Return: 0
+ */
+#include<stdio.h>
+int main()
+{
+char ch;
+
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+ if((ch < 'q'||ch > 'q') && (ch > 'e'||ch < 'e')
+putchar(ch);
+putchar('\n');
+return 0;
+}
