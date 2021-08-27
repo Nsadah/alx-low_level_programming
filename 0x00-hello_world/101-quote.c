1beee897fdbde2eed82d7c080cb4b962e62d9e1c
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-08-27 12:48:04 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,22 +1,12 @@
+#include <stdio.h>
+
 /**
-* main - Entry
-*
-* Return:1
-*/
-#include<stdio.h>
-int main(void) /*main prints text*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-char s[] ="and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-long 1 = 59;
-long fd = 1;
-long syscall = 1;
-long ref = 0;
-_asm_ l "sys call"; *
-: "a" (ret);
-: "a" (sys call);
-"D" (fd);
-"S" (s);
-"d" (1); *
-/* Description:printing */
-return (1);/* Description: This returns 0 */
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
 }
