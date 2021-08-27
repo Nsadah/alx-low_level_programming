32a9436dd74f644fd5c0742f54021869639d0c34
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-08-28 01:50:00 +0300

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,57 +1,33 @@
 /**
-	 * main - prints to string
-	 * Description: Prints alphabet in lowercase
-	 * Return: 0
-	 */
-
-/**
-	 * main - prints to string
-	 * Description: Prints alphabet in lowercase
-	 * Return: 0
-	 */
-
-/**
-	 * main - prints to string
-	 * Description: Prints alphabet in lowercase
-	 * Return: 0
-	 */
-
-/**
-	 * main - prints to string
-	 * Description: Prints alphabet in lowercase
-	 * Return: 0
-	 */
-
-#include<stdio.h>  
+* main - prints combination
+* Description: Prints combination of three
+* Return: 0
+*/
+#include<stdio.h>
 #include <unistd.h>
-int main(void)  
+int main(void)
+{
+int i, j, k;
+for(i = '0'; i <= '9'; i++)
 {  
-    int i, j, k;  
-  
-    for(i = '0'; i <= '9'; i++)  
-    {  
-    for(j = '0'; j <= '9'; j++) 	 
-	{  
-	for (k =0; k <= '9'; k++)
-    {
-	if(i < j && j < k)
-	{
-	putchar (i);
-	putchar (j);
-	putchar (k);
-
+for(j = '0'; j <= '9'; j++) 	 
+{  
+for (k =0; k <= '9'; k++)
+{
+if(i < j && j < k)
+{
+putchar (i);
+putchar (j);
+putchar (k);
 if (i!='7'|| ((i == '7' )&& (j !='8')) || (j != '8' ||((j == '8') && k !='9')))
 {
 putchar (',');
 putchar (' ');
 }
-}		
+}	
 }
 }
 }
 putchar('\n');
 return 0;
 }
-		  
-    
-
