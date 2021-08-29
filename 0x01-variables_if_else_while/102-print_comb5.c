6c7ef843ec3829e177a390722816b94ed2688ce8
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-08-30 02:40:45 +0300

    Update 102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+/**
+* main - prints to string
+* Description: Prints alphabet in lowercase
+* Return: 0
+*/
+#include<stdio.h>
+#include <unistd.h>
+int main(void)
+{
+int a,b,c,d;
+for (a = '0'; a <= '9'; a++)
+{
+for (b = '0'; b <= '9'; b++)
+{
+for (c = '0'; c <= '9'; c++)
+{
+for(d = '0'; d <= '9'; d++)
+{
+if(!(a==b || a==c || a==d || b==c || b==d || c==d))
+{
+putchar (a);
+putchar (b);
+putchar (' ');
+putchar (c);
+putchar (d);
+putchar (',');
+putchar (' ');
+}		
+}
+}
+}
+}
+putchar ('\n');
+return (0);
+}
