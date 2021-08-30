3a84941f203981f3753db49be891e8b55d1a7c37
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-08-30 15:00:43 +0300

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,15 +1,9 @@
 /**
-* main - prints to string
-* Description: Prints alphabet in lowercase
-* Return: 0
+*main-prints to string
+*Description: Prints double combinations
+*Return: 0
 */
-
 #include <stdio.h>
-/**
- * main - Prints 3 combination of numbers
- *
- * Return: Always (Success)
- */
 int main(void)
 {
 int c, i, k, j;
