e9fb523adedca63a17658fec3cde098d1f5e3e81
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-08-30 15:38:37 +0300

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,39 +1,33 @@
 /**
-*main - prints double didgit comination
-*description: Prints double digit combinations
-*Return: 0
-*/
-#include <stdio.h>
-int main(void)
-{
-int a, b, c, d;
-for (a = 48; a <= 57; a++)
-{
-for (b = 48; b <= 57; b++)
-{
-for (c = 48; c <= 57; c++)
-{
-for (d = 48; d <= 57; d++)
-if (((c + d) > (a + b) && c >= a) || a < c)
-{
-putchar (a);
-putchar (b);
-putchar (' ');
-putchar (c);
-putchar (d);
-if (a + b + c + d == 227 && a == 57)
-{
-break;
-}
-else
-{
-putchar (',');
-putchar (' ');
-}
-}
-}
-}
-}
-putchar ('\n');
-return (0);
-}
+	* main - prints to string
+	* Description: Prints alphabet in lowercase
+	* Return: 0
+	*/
+	#include <stdio.h>
+	#include <unistd.h>
+	int main(void)
+	{
+	int i, j, k;
+	for (i = '0'; i <= '9'; i++)
+	{
+	for (j = '0'; j <= '9'; j++)
+	{
+	for (k = 0; k <= '9'; k++)
+	{
+	if (i < j && j < k)
+	{
+	putchar (i);
+	putchar (j);
+	putchar (k);
+	if (i != '7' || ((i == '7') && (j != '8')) || ((j == '8') && k != '9'))
+	{
+	putchar (',');
+	putchar (' ');
+	}
+	}
+	}
+	}
+	}
+	putchar ('\n');
+	return (0);
+	}
