827914fb218b8eed03584333a81b0583d664d9a5
Nsadah <michealnsadahal@gmail.com>
2021-08-31 02:52:37 -0700

    C

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+/**
+ * main - prints to string
+ * Description: Prints alphabet in lowercase
+ * Return: Always (Success)
+ */
+
+#include <stdio.h>
+int addNumbers(int a, int b);         // function prototype
+
+int main(void)
+{
+int ch;
+/* print lowercase letters */
+for (ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
+putchar ('\n');
+}
+void print_alphabet(void)         // function definition   
+{
+int ch;  
+return print_alphabet;        // return statement
+}
