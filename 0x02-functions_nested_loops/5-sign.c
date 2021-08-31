fa9150544a0ba519a91470f9a1a9ced400cb44bb
Nsadah <michealnsadahal@gmail.com>
2021-08-31 09:32:10 -0700

    C

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+/**
+ * print_sign - prints to int
+ * Description: int c is a variable
+ * Return: 1
+ */
+#include "main.h"
+#include<ctype.h>
+int print_sign(int n) /* c is a variable*/
+{
+if (n > 0)
+{
+_putchar ('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else
+{
+_putchar ('-');
+return (-1);
+}
+}
