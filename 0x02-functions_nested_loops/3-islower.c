52a19537613c4b03e6071340d6eccb32f48d5193
Nsadah <michealnsadahal@gmail.com>
2021-08-31 08:18:45 -0700

    C

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+/**
+* print_alphabet - prints to string
+* Description: Prints alphabet in lowercase
+* void print_alphabet(void)
+*/
+#include "main.h"
+#include<ctype.h>
+int _islower(int c)
+{
+if (islower(c) )
+{  
+return 1;
+}
+else
+{
+return (0);   
+}
+}
