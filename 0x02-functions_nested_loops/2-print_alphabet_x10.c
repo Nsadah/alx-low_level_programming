de56986fd24d74cf6953728e9a56abbff4173543
Nsadah <michealnsadahal@gmail.com>
2021-08-31 07:08:26 -0700

    C

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+/**
+ * print_alphabet - prints to string
+ * Description: Prints alphabet in lowercase
+ * void print_alphabet(void)
+ */
+#include "main.h"
+void print_alphabet(void)
+{
+int x;
+char ch;
+for (x = 0; x <= 10; x++)
+{
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+_putchar(ch);
+}
+_putchar('\n');
+}
+return;
+}
