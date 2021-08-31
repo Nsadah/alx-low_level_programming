ec6845128e6948cc4981a0693d63d3a42efd63b7
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-08-31 13:32:41 +0300

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,23 @@
 /**
- * void print_alphabet is the prototype
  * main - prints to string
  * Description: Prints alphabet in lowercase
  * Return: Always (Success)
  */
+
 #include <stdio.h>
-int main(void)
-{
-int ch;
-/* print lowercase letters */
-for (ch = 'a'; ch <= 'z'; ch++)
-putchar(ch);
-putchar ('\n');
-}
 void print_alphabet(void)
 {
-int ch;
-return int ch; 
+int ch;  
+return ch;
+}
+int main(void)
+{
+int x;
+void print_alphabet(x);
+for (x = 'a'; x <= 'z'; x++)
+{
+putchar(x);	
+}
+putchar ('\n');
+return;
 }
