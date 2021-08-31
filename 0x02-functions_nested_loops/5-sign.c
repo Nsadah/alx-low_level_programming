227b9e0d77c5a25cd474e5266f9a29f1bf291137
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-08-31 19:25:00 +0300

    Update 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+/**
+ * print_sign - prints to int
+ * Description: int c is a variable
+ * Return: 1
+ */
+#include "main.h"
+#include<ctype.h>
+int print_sign(int n); /* c is a variable*/
+{
+if (n > 0)
+{
+putchar ('+');
+return (1);
+}
+else if (n ==0)
+{
+putchar('0');
+return (0);
+}
+else
+{
+return (-1);
+}
+}
