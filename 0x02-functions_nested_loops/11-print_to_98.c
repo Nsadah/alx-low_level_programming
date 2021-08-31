a95ec30add6426770f1cae8b330c02900f6778f0
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-09-01 02:56:28 +0300

    Update 11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,15 @@
+	/**
+ * pprint_to_98 - prints to int
+ * Description: Prints alphabet in lowercase
+ *@n: the integer value it receives
+ * void print_to_98(int n)
+ */
+#include "main.h"
+void print_to_98(int n)
+{
+for (a = 0 ; a <= 98; a++ ) 
+{
+printf("%d, ",a);
+}
+printf("\n");
+}
