9b139db09fb7a9b9d08b235361ab9b107234761c
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-08-31 18:16:54 +0300

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,18 +1,1 @@
-/**
- * print_alphabet - prints to string
- * Description: Prints alphabet in lowercase
- * void print_alphabet(void)
- */
-#include "main.h"
-#include<ctype.h>
-int _islower(int c)
-{
-if ( islower(c) )
-{  
-return 1;
-}
-else
-{
-return 0;   
-}
-}
+
