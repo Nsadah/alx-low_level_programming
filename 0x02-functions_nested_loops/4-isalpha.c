09901028c274e734d5eea100bc158f2be2c5ebf3
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-08-31 19:08:58 +0300

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,8 +1,8 @@
 /**
- * _isalpha - prints to string
- * Description: int c is a variable
- * Return: 1
- */
+* _isalpha - prints to string
+* Description: int c is a variable
+* Return: 1
+*/
 #include "main.h"
 #include<ctype.h>
 int _isalpha(int c) /* c is a variable*/
