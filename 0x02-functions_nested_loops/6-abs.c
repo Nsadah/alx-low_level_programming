fb47c253fa1b0b6192bbe715b9929fe77d1aaeb1
Nsadah <michealnsadahal@gmail.com>
2021-08-31 09:38:12 -0700

    C

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+/**
+ * int _abs(int) - prints to int
+ * Description: prints absolute value
+ * Return: 0
+ */
+#include "main.h"
+#include<ctype.h>
+int _abs(int) /* c is a variable*/
+{
+	int n;
+if (n < 0)
+{
+	n = (-1) * n;
+_putchar (n;
+
+}
+_putchar ('\n');
+return (0);
+}
