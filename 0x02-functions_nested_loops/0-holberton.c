6664a024ea6e3ed8834a01361887f6ef5c58ecd2
Nsadah <michealnsadahal@gmail.com>
2021-08-31 02:24:46 -0700

    C

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,12 @@
+/**
+ * main - prints to string
+ * Description: Prints positive, negative or zero
+ * Return: 0
+ */
+#include <stdio.h>
+int main(void)
+{
+printf("Holberton");
+putchar ('\n');
+return (0);
+}
