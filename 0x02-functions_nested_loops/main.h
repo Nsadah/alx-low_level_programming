447bcb00318fbff227e13291070f5d2d59985fd1
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-08-31 15:20:16 +0300

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,12 +11,5 @@
 #ifndef MAIN_H
 #define MAIN_H
 int _putchar(char c);
-void print_alphabet(void)
-{
-int ch; 
-for (ch = 'a'; ch <= 'z'; ch++)
-{
-putchar(ch);	
-} 
-}
+void print_alphabet(void);
 #endif
