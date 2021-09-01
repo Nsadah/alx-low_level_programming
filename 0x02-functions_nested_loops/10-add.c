3c7545d3977804b47928192988ed973c31094f0a
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-09-01 16:57:16 +0300

    Update 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+/**
+ * int add - prints to int
+ * Description: Prints alphabet in lowercase
+ *@n: the integer value it receives
+ * void print_to_98(int n)
+ */
+#include "main.h"
+#include <stdio.h>
+int add(int a, int b)
+{
+int r = a + b;
+printf("%d", r);
+return r;
+}
