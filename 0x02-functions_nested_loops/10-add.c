862a0da4054e0b9fcc7e9d48e0e3a5f13d9ab11f
Nsadah <michealnsadahal@gmail.com>
2021-09-01 07:06:35 -0700

    C

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+/**
+ * add - prints to int
+ * Description: Prints alphabet in lowercase
+ *@a: the integer value it receives
+ *@b: the integer it rescieves
+ *int add(int a, int b)
+ ** Return: 0 (Success)
+ */
+#include "main.h"
+#include <stdio.h>
+int add(int a, int b)
+{
+int r = a + b;
+return (r); 
+}
