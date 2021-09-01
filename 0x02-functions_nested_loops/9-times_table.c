43382483d49da482885aff353fd28827a20557d9
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-09-01 15:26:51 +0300

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,22 +8,17 @@
 #include <stdio.h>
 void times_table(void)
 {
-int i,j;
-	
-     for(i=0;i<=9;i++)
-	 {
-    
-    for(j=0;j<=9;j++)
-	{
-        printf("%2d",(i*j));
-        
-		if(j%9 != 0 || j == 0)
-        {
-        printf(", ");
-        }
-    }
-    printf("\n\n");
-     }
-
+int i,j;	
+for(i=0;i<=9;i++)
+{
+for(j=0;j<=9;j++)
+{
+printf("%2d",(i*j));        
+if(j%9 != 0 || j == 0)
+{
+printf(", ");
 }
-
+}
+printf("\n");
+}
+}
