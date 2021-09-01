78a5cfda100dd58ddfd8ac1291822e1ee136a869
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-09-01 17:04:09 +0300

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,13 +3,13 @@
  * Description: Prints alphabet in lowercase
  *@a: the integer value it receives
  *@b: the integer it rescieves
- * int add(int a, int b)
- *return: 0 (Success)
+ *int add(int a, int b)
+ ** Return: 0 (Success)
  */
 #include "main.h"
 #include <stdio.h>
 int add(int a, int b)
 {
 int r = a + b;
-return (r);
+return (r); 
 }
