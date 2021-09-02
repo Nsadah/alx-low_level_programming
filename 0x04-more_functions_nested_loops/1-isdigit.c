4b4724435c5ecd80e328b614396adeee19984bf2
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-09-03 01:08:55 +0300

    Update 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,8 +1,8 @@
 /**
-* _isdigit: prints numbers
+* _isdigit - Entry point
 * Description: prints numbers
-* @c: the integer value it receives
-* Return: Always(success)
+*@c: the integer value it receives
+* Return: 1 if true. 0 if false.
 */
 #include "main.h"
 int _isdigit(int c)
