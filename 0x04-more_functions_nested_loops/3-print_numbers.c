611e63ad78e00420e2c5c3a11013e43cbd549a50
Nsadah <michealnsadahal@gmail.com>
2021-09-02 08:55:21 -0700

    C

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+
+/**
+ * print_numbers - prints to string
+ * Description: Prints 1 to 9
+ * void print_numbers(void)
+ */
+#include "main.h"
+void print_numbers(void)
+{
+char i;
+for (i = 0; i <= i; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+return;
+}
