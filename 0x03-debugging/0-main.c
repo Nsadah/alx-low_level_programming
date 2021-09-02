ca8b0b9b65530e3bee4f0dc9cb7dd0fde32ed43f
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-09-02 15:21:59 +0300

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,28 @@
-#include "main.h"
-
 /**
-* main - tests function that prints if integer is positive or negative
-* Return: 0
-*/
-
-int main(void)
+ * print_sign - prints to int
+ * Description: int n is a variable
+ *@n: the integer value it receives
+ * Return: 1
+ */
+#include "main.h"
+int positive_or_negative(void)
 {
-int i;
-
-i = 98;
-positive_or_negative(i);
-
+  int i;
+  /* refer int i*/
+srand(time(0));
+i = rand() - RAND_MAX / 2;
+/* your code goes there */
+if (i > 0)
+{
+printf("%d is positive\n", i);
+}
+else if (i == 0)
+{
+printf("%d is zero\n", i);
+}
+else
+{
+printf("%d is negative\n", i);
+}
 return (0);
 }
