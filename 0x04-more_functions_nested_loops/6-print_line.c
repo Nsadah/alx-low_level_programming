36c0d7b21fba9408ff15270ff9cc866892fdc5c8
Nsadah <michealnsadahal@gmail.com>
2021-09-02 11:39:36 -0700

    C

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+/**
+ * print_most_numbers - prints to int
+ * Description: Prints numbers
+ * void print_most_numbers(void)
+ */
+#include <stdio.h>
+int main()
+{
+int i;
+int l;
+for (l = 1; l <= 10; l++)
+{
+for (i = 0; i <= 14; i++)
+{
+putchar (i + '0');
+}
+putchar ('\n');
+}
+}
---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void print_line(int n)
+{
+
+  int i=0;
+  for (i =0; I <=100   {
+        printf("_");
+    }
+}
