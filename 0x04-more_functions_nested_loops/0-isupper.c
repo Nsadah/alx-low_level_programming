e91b8b6c67ee65c6ebdb41a406d838bfcfecabb5
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-09-02 16:38:55 +0300

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,1 +1,22 @@
-0;136;0c
+/**
+* print_last_digit - prints to int
+* Description: prints last digit
+*@n: the integer value it receives
+* Return: 0
+*/
+#include "main.h"
+#include <stdio.h>
+int _isupper(int c)
+{
+for (c = 'A'; c <= 'Z'; c++)
+{
+if (c => 'A' || c <= 'Z')
+{
+_putchar (C);
+return (1);
+}
+else
+{
+return (0);
+}
+}
