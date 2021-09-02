5a5eeb7c0eeee6d169d88172fdbeb510a18d53f5
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-09-02 18:23:57 +0300

    Update 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,19 +1,22 @@
-/**
-* _isdigit- Entry point
-* Description: prints alphabet
-*@c: the integer value it receives
-* Return: 1 if true. 0 if false.
-*/
+
 #include "main.h"
-int _isdigit(int c)
+/**
+ * _islower - Entry point
+ * Description: checks is a character lowercase
+ * @c: the integer value it receives
+ * Return: 1 if true. 0 if false.
+ */
+int _islower(int c)
 {
 int i;
+
 for (i = 0; i <= 9; i++)
 {
 /* refer int c*/
 if (c == i)
 {
 return (1);
+}
 }
 return (0);
 }
