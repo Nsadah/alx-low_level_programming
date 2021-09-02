3e4721374f2a9cbce97be91e6da960f320f1f5c8
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-09-02 17:27:15 +0300

    Update 1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,26 @@
+/**
+* int _isupper - prints to int
+* Description: prints alphabet
+*@c: the integer value it receives
+* Return: 0
+*/
+#include "main.h"
+#include <stdio.h>
+int _isupper(int c)
+{
+for (c = 0; c <= 9; c++)
+{
+if (c >= 0 || c <= 9)
+{
+_putchar (c);
+return (1);
+}
+else
+{
+_putchar(c);
+return (0);
+}
+}
+}
+_putchar('\n');
+}
