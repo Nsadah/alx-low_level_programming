f5ea107f8d26e97e3b5e29dcfdbeb22aacf3ef33
Nsadah <michealnsadahal@gmail.com>
2021-09-02 08:50:31 -0700

    C

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+/**
+ * mul - prints to int
+ * Description: Prints multiplication
+ *@a: the integer value it receives
+ *@b: the integer it rescieves
+ *int add(int a, int b)
+ ** Return: 0 (Success)
+ */
+#include "main.h"
+#include <stdio.h>
+int mul(int a, int b)
+{
+int r = a * b;
+return (r);
+}
