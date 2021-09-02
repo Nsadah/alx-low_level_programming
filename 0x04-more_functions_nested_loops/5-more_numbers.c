af53bf128cedc7c2ab161c1442710b7c0fe9d2ce
Nsadah <michealnsadahal@gmail.com>
2021-09-02 09:48:51 -0700

    C

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+/**
+ * print_most_numbers - prints to int
+ * Description: Prints numbers
+ *void more_numbers(void)
+ */
+#include "main.h"
+void more_numbers(void)
+{
+int i, l;
+for (l = 1; l <= 10; l++)
+{
+for (i = 0; i <= 14; i++)
+{
+_putchar (i);
+}
+_putchar ('\n');
+}
+}
