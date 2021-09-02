799a4c17785f8f4f55b33b24c1ca155442f9f83a
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-09-02 18:14:55 +0300

    Update 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,26 +1,20 @@
 /**
-* int _isdigit - prints to int
+* int _isupper - Entry point
 * Description: prints alphabet
 *@c: the integer value it receives
-* Return: 0
+* Return: 1 if true. 0 if false.
 */
 #include "main.h"
-#include <stdio.h>
-int _isdigit(int c)
+int _isupper(int c)
 {
-for (c = 0; c <= 9; c++)
+int i = 0;
+for (i = 0; i <= 9'; i++)
 {
-if (c >= 0)
+/* refer int c*/
+if (c == i)
 {
-_putchar (c);
 return (1);
 }
-else
-{
-_putchar(c);
+}
 return (0);
-}
-}
-}
-_putchar('\n');
 }
