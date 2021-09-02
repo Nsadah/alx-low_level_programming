1e4ff383ba31bd1e29407704d8135282973dd409
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-09-02 15:57:06 +0300

    Update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,6 +3,18 @@
 
 #include <stdio.h>
 
-void positive_or_negative(int i);
-
+void positive_or_negative(int i)
+{
+if (n > 0)
+{
+printf("%d is positive\n", n);
+}
+else if (n == 0)
+{
+printf("%d is zero\n", n);
+}
+else
+{
+printf("%d is negative\n", n);
+}
 #endif /* MAIN_H */
