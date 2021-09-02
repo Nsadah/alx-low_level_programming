8b32a41ea693c7fa094b8756a479910949f63ae7
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-09-02 15:40:47 +0300

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -5,25 +5,18 @@
  * Return: 1
  */
 #include "main.h"
-#include <time.h>
-#include <stdlib>
-void positive_or_negative(void)
-{
-  int i;
-  /* refer int i*/
 
-/* your code goes there */
-if (i > 0)
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
+int main(void)
 {
-printf("%d is positive\n", i);
-}
-else if (i == 0)
-{
-printf("%d is zero\n", i);
-}
-else
-{
-printf("%d is negative\n", i);
-}
+int i;
+
+i = 98;
+positive_or_negative(i);
+
 return (0);
 }
