6ae23efd672172c62350bd437e8cb61cdf26b0aa
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-09-04 01:01:34 +0300

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,1 +1,23 @@
-0;136;0c
+/**
+* print_triangle - Entry point
+* Description: prints triangle
+*@size: the integer value it receives
+* Return: 1 if true. 0 if false.
+*/
+#include "main."
+void print_triangle(int size);
+{
+int i, j, k;
+for(i = 1;i <= size;i++)
+{
+for(j = size;j > i;j--)
+{
+printf(" ");
+}
+for(k = 1;k <= j;k++)
+{
+printf("*");
+}
+printf("\n");
+}
+}
