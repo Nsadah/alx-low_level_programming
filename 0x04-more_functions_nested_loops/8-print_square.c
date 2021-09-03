e5f48491c20b40610476b998dbe02f717e915d23
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-09-03 03:32:38 +0300

    Update 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,1 +1,18 @@
-
+ /**
+* print_square - Entry point
+* Description: prints numbers
+*@size: the integer value it receives
+*/
+#include "main.h"
+void print_square(int size)
+{
+int i, w;
+for(size = 1; size <= w; i++)
+{
+for(i = 1; j <= w; j++)
+{
+_putchar("#");
+}
+_putchar("\n");
+}
+}
