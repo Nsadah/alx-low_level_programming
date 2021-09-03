bd6db30e498ccab377c2926b259e232b5d575fbb
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-09-03 15:49:12 +0300

    Update main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,19 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdio.h>
-int _putchar(char c);
+#include <unistd.h>
+/**
+*_putchar writes the characyer c yto stdout
+*@c: The character it receives
+*
+*Return: On success 1.
+*On error, -1 is returned and error is set appropriately
+*/
+int _putchar(char c)
+{
+  return (write (1, &c, 1));
+}
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
