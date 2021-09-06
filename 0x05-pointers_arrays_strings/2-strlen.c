ec44738f63577df7af288d575707638c3a73df20
Nsadah <michealnsadahal@gmail.com>
2021-09-06 13:58:50 -0700

    Pointers

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,17 +1,18 @@
 /**
-* int _strlen - prints to int
-* Description: Returns length of string
+* _strlen - prints to int
+* Description: Prints length of string
 * @s: the value it receives
 * int _strlen(char *s)
+* Return: Always (Success)
 */
 #include "main.h"
 int _strlen(char *s)
 {
 unsigned int count = 0;
-while(*s!='\0')
+while (*s != '\0')
 {
 count++;
 s++;
 }
-return count;
+return (count);
 }
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+/**
+* _puts - prints to string
+* Description: Prints to  string
+* @str: the value it receives
+* void _puts(char *str)
+* Return: Always (Success)
+*/
+#include "main.h"
+void _puts(char *str)
+{
+}
