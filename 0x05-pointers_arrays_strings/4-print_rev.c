183f016e904d9e7ac205c8e6b533593abff8eefc
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-09-08 02:29:03 +0300

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,10 +11,10 @@
 void print_rev(char *s)
 {
 int i;
-for (i=0; s[i]!='\0'; i++);
-for (i--; i>=0; i--)
+for (i = 0; s[i] != '\0'; i++);
+for (i--; i >= 0; i--)
 {
-printf ("%c",s[i]);
-} 
-printf ("\n");
+printf("%c", s[i]);
+}
+printf("\n");
 }
