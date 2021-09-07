76bf866ca4cb495d735fc2099daf11fd84c5ae7d
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-09-08 02:43:40 +0300

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,14 +1,14 @@
 /**
-* _puts2 - prints to string
+* puts2 - prints to string
 * Description: Prints to  string
 * @str: the value it receives
-* void _puts2(char *str)
+* void puts2(char *str)
 * Return: Always (Success)
 */
 #include <stdio.h>
 #include <string.h>
 #include "main.h"
-void _puts2(char *str)
+void puts2(char *str)
 {
 long unsigned int i;
 for(i = 0; i < strlen(str); i +=2)
