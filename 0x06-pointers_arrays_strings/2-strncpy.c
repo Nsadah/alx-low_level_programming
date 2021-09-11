644c671620304ed6b37bf59b7b3e1a03da331f5a
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-09-12 02:24:48 +0300

    Create 2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,17 @@
+/**
+* _strncpy - prints to string
+* Description: prints to string
+*@dest: the value it receives
+*@src: the value it receives
+*@n: the value it receives
+*char *_strncpy(char *dest, char *src, int n)
+*/
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+char *_strncpy(char *dest, char *src, int n)
+{
+for(n=0;src[n]=dest[n];n++);
+src[n]='\0';	
+}
