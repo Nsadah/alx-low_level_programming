a0d150b740e48fc8b55e2a477001d1c34c9be53d
Nsadah <michealnsadahal@gmail.com>
2021-09-13 18:15:21 -0700

    C

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,26 +1,19 @@
 /**
-* _strchr - Fills the first @n bytes of the memory area
+* _memcpy - Copies @n bytes of the memory area
 *          pointed to by @s with the constant byte @b.
 * Description: Prints to string
-*@s: the value it receives
-*@c: the value it receives
-* char *_strchr(char *s, char c)
-* Return: Null if no char is located, else,
-* return first occurance of character c is a string s
+*@dest: the value it receives
+*@src: the value it receives
+*@n: the value it receives
+* char *_memcpy(char *dest, char *src, unsigned int n)
+* Return: dest
 */
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-char *_strchr(char *s, char c)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
-int i;
-for (i = 0; s[i] != '\0'; i++)
-{
-if (s[i] == c)
-{
-return (s + i);
-}
-}
-return ('\0');
+strcpy(dest, " ");
+_memcpy(dest, src, strlen(src) + 1);
 }
