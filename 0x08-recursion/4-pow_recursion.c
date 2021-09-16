79d91c3591dde5944d29e9c7fd412d28aed4f313
Nsadah <michealnsadahal@gmail.com>
2021-09-15 17:19:43 -0700

    C

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+/**
+* _pow_recursion - returns the value of x raised to the power of y
+* Description: Prints factorials
+*@x: the value it receives
+*@y: the value it receives
+* Return: Always (Success)
+*/
+#include "main.h"
+#include <stdio.h>
+#include <math.h>
+int _pow_recursion(int x, int y)
+{
+double result;
+if(y < 0)
+return (-1);
+else
+return x * pow(x, y - 1);
+}
