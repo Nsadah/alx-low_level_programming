7e3fe94b0218299c801592749deb903c69312e6c
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-09-23 02:29:38 +0300

    Create 100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,17 @@
+/**
+* _atoi - prints to string
+* Description: Prints to string
+*@dest: the value it receives
+*@src: the value it receives
+* int _atoi(char *s)
+* Return: Always (Success)
+*/
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int _atoi(char *s)
+{
+int x = _atoi(s);
+printf("%d\n", x);
+}
