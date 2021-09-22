03daad692f089c54e10dfe3ec1c1226e86f37d1b
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-09-23 01:22:04 +0300

    Update create_static_lib.sh

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
-echo
-'
-ar -rc liball.a 0-isupper.o
-arc -rc liball.a 0-memset.o
-'
-> liball.a
+echo arc -rc liball.a 0-isupper.o > liball.a
+echo arc -rc liball.a 0-memset.o > liball.a
+echo arc -rc liball.a 0-strcat.o > liball.a
+echo arc -rc liball.a 100-atoi.o > liball.a
+echo arc -rc liball.a 1-isdigit.o > liball.a
+echo arc -rc liball.a 1-memcpy.o > liball.a
+echo arc -rc liball.a 1-strncat.o > liball.a
+echo arc -rc liball.a 2-strchr.o > liball.a
+echo arc -rc liball.a 2-strlen.o > liball.a
+echo arc -rc liball.a 2-strncpy.o > liball.a
+echo arc -rc liball.a 3-islower.o > liball.a
+echo arc -rc liball.a 3-puts.o > liball.a
+echo arc -rc liball.a 3-strcmp.o > liball.a
+echo arc -rc liball.a 3-strspn.o > liball.a
+echo arc -rc liball.a 4-isalpha.o > liball.a
+echo arc -rc liball.a 4-strpbrk.o > liball.a
+echo arc -rc liball.a 5-strstr.o > liball.a
+echo arc -rc liball.a 6-abs.o > liball.a
+echo arc -rc liball.a 9-strcpy.o > liball.a
+echo arc -rc liball.a _putchar.o > liball.a
+
