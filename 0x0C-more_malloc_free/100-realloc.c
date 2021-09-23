1e3ef41d61c021f488d48b80ed83b680176e866b
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-09-23 22:08:59 +0300

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,28 +12,27 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *s;
-
-	if (new_size > old_size)
-	{
-		s = malloc(new_size);
-		free(ptr);
-		return (s);
-	}
-	if (new_size == old_size)
-	{
-		return (ptr);
-	}
-	if (ptr == NULL)
-	{
-		s = malloc(new_size);
-		free(ptr);
-		return (s);
-	}
-	if (new_size == 0 && ptr != NULL)
-	{
-		free(ptr);
-		return (NULL);
-	}
-	return (ptr);
+char *s;
+if (new_size > old_size)
+{
+s = malloc(new_size);
+free(ptr);
+return (s);
+}
+if (new_size == old_size)
+{
+return (ptr);
+}
+if (ptr == NULL)
+{
+s = malloc(new_size);
+free(ptr);
+return (s);
+}
+if (new_size == 0 && ptr != NULL)
+{
+free(ptr);
+return (NULL);
+}
+return (ptr);
 }
