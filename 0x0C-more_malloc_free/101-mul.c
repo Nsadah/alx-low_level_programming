e734d74a8ea6f83e2db6f71830407f6b010759bb
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-09-23 23:27:31 +0300

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -22,12 +22,9 @@ for (num2 = 0; argv[num1][num2] != '\0'; num2++)
 if (argv[num1][num2] > 57 || argv[num1][num2] < 48)
 {
 printf("Error\n");
-<<<<<<< HEAD
 exit(98);
 }
-=======
-exit(98); }
->>>>>>> 05964c14b0b24492b8c7642ca992fca263186ebd
+exit(98);
 }
 }
 mul = atol(argv[1]) *  atol(argv[2]);
