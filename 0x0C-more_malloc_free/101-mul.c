f706f93d409d7f26a5c59fa9a74e5dc431ab36c9
Nsadah <michealnsadahal@gmail.com>
2021-09-23 12:57:44 -0700

    C

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,6 +1,6 @@
+
 #include <stdio.h>
 #include <stdlib.h>
-#include "holberton.h"
 /**
 * main - multiply 2 numbers as arguments
 * @argc: number of arguments
@@ -10,21 +10,24 @@
 int main(int argc, char *argv[])
 {
 unsigned long mul;
-int i, j;
-	if (argc != 3)
-	{ printf("Error\n");
-	exit(98); }
-	for (i = 1; i < argc; i++)
-	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (argv[i][j] > 57 || argv[i][j] < 48)
-			{  printf("Error\n");
-			exit(98); }
-		}
-
-	}
-	mul = atol(argv[1]) *  atol(argv[2]);
-	printf("%lu\n", mul);
+int num1, num2;
+if (argc != 3)
+{
+printf("Error\n");
+exit(98);
+}
+for (num1 = 1; num1 < argc; num1++)
+{
+for (num2 = 0; argv[num1][num2] != '\0'; num2++)
+{
+if (argv[num1][num2] > 57 || argv[num1][num2] < 48)
+{
+printf("Error\n");
+exit(98);
+}
+}
+}
+mul = atol(argv[1]) *  atol(argv[2]);
+printf("%lu\n", mul);
 return (0);
 }
