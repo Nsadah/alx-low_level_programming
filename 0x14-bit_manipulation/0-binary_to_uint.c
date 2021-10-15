4404b21bc2174691bf9d4e3bd19b1c4a2f0767a4
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-10-15 13:45:35 +0300

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,15 +8,15 @@ unsigned int binary_to_uint(const char *b)
 {
 unsigned int res = 0;
 int base = 1, i = 0;
-  if (b == NULL)
-  {
-		return (0);
-  }
-	while (b[i + 1])
+while (b[i + 1])
 {
 if (b[i] != '0' && b[i] != '1')
 return (0);
 i++;
+if (b == NULL)
+  {
+		return (0);
+  }
 }
 while (i >= 0)
 {
