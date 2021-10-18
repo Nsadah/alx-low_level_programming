9e097e450ccf0a31b676020ad42916c6b6c237bf
Nsadah <88324392+Nsadah@users.noreply.github.com>
2021-10-18 19:59:05 +0300

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -27,19 +27,3 @@ int append_text_to_file(const char *filename, char *text_content)
 	close(fd);
 	return (1);
 }
-
-/**
-  * _strlen - Returns the length of a string
-  * @s: String to count
-  *
-  * Return: String length
-  */
-int _strlen(char *s)
-{
-	int c = 0;
-
-	while (s[c])
-		c++;
-
-	return (c);
-}
